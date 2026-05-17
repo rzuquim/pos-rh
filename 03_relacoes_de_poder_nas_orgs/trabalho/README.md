fcce4e23935510391761f0a6c515249a7e971675
Rafael Zuquim <rzuquim@gmail.com>
2026-05-17 19:53:15 -0300

    estrutura dos slides do trabalho de poderes

---
+++ 03_relacoes_de_poder_nas_orgs/trabalho/README.md
@@ -0,0 +1,28 @@
+# Artigo e Apresentação
+
+Discutir sobre o ambiente de trabalho e relações de poder
+
+Contextualizar ambiente e rotinas, a cena de trabalho.
+
+Escrever um artigo e uma apresentação.
+
+# Divisão do trabalho
+
+- PPT - **Rafa**
+- Título
+- Resumo/Intro - Naomi
+- Palavras-chave - Naomi
+- Referencial teórico - Manu
+- Metodologia - Naomi
+- Resultados e discussão
+  - Descrever o ambiente/cena - **Rafa**
+  - Analise - Naomi
+  - Conclusão - Naomi
+
+Resumo/contexto
+
+# Notas:
+
+- Reunião com Grupo: https://docs.google.com/document/d/1iaxCceAF-SB4I4j4OMCQPFjlWggVlLW-n04vIehg3yI/edit?tab=t.0
+
+- Pesquisa da Manu: https://docs.google.com/document/d/19eLOMcDtzKJvXVncVCNlKAb9kBpUMC4m4BElIPayIes/edit?pli=1&tab=t.0
---
+++ 03_relacoes_de_poder_nas_orgs/trabalho/slides/src/main.ts
@@ -0,0 +1,32 @@
+import "./style.css";
+
+import "reveal.js/reveal.css";
+import "reveal.js/theme/black.css";
+
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown";
+
+let slidesDOM = document.querySelector(".slides");
+if (!slidesDOM) {
+  console.error("Could not find slides container.");
+}
+
+let slideModules = import.meta.glob("../slides/*.html", {
+  query: "?raw",
+  import: "default",
+  eager: true,
+});
+
+let sortedSlidePaths = Object.keys(slideModules).sort();
+
+let allSlidesHTML = "";
+for (const path of sortedSlidePaths) {
+  allSlidesHTML += slideModules[path] as string;
+}
+
+slidesDOM.innerHTML = allSlidesHTML;
+
+let deck = new Reveal({
+  plugins: [Markdown],
+});
+deck.initialize();
